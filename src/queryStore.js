b969902ef8fed79367c63461354f161aa51731b3
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-02-23 23:30:07 +0100

    CLI based scraping

--- src/queryStore.js
+++ src/queryStore.js
@@ -5,23 +5,23 @@ import logger from './logger'
 import { Source, parse } from "graphql"
 import { queryDirectiveResolver } from "./queryDirectiveResolver";
 import { queryDirectives } from "./directives";
-import schema from "./schema";
 
-const loadQueryStore = async () => {
+const loadQueryStore = async (schema) => {
   const files = glob.sync(process.env.PWD + "/queries/**/*.gql")
   const queries = await Promise.all(files.map(async (file) => {
     const queryString = readFileSync(file).toString();
-    const source = new Source(queryString, basename(file));
+    const queryName = basename(file, '.gql');
+    const source = new Source(queryString, queryName);
     const queryDocument = await parse(source);
     return await Promise.all(queryDocument.definitions.map(async (definition) => {
       const resolver = queryDirectiveResolver(definition, queryDirectives, schema);
       if (!resolver) return;
-      const localContext = {};
-      const config = await resolver(Promise.resolve, {}, localContext);
+      const config = await resolver(Promise.resolve, {}, {});
       logger.debug('Config provided by Query directives:', JSON.stringify(config));
       return {
         config,
-        queryString
+        queryString,
+        queryName
       }
     }))
   }))
