87dedcd8ff003a4905b21fc021068d4b39db7973
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-02-23 22:28:46 +0100

    Extract queryDirectiveMiddleware

--- src/app.js
+++ src/app.js
@@ -2,45 +2,18 @@ import express from 'express';
 import morgan from 'morgan';
 import logger from './logger'
 import bodyParser from 'body-parser';
-import { getGraphQLParams } from 'express-graphql'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import {
-  Source,
-  parse
-} from 'graphql'
 import urlLoader from './browser'
-import { queryDirectiveResolver } from './queryDirectiveResolver'
-import { queryDirectives } from "./directives";
+import queryDirectiveMiddleware from "./queryDirectiveMiddleware";
 import schema from './schema';
 
 const app = express();
 app.disable('x-powered-by');
-
 app.use(morgan('combined', { stream: logger.stream }));
-
-const queryDirectiveMiddleware = async (request, response, next) => {
-  await getGraphQLParams(request).then(async graphQLParams => {
-    const source = new Source(graphQLParams.query, 'GraphQL request');
-    try {
-      const documentAST = await parse(source);
-      const resolver = queryDirectiveResolver(documentAST.definitions[0], queryDirectives, schema);
-      if (!resolver) return;
-      const config = await resolver(Promise.resolve, {}, {});
-      logger.debug("Context provided by Query directives:", JSON.stringify(config));
-      response.locals.queryConfig = config;
-    } catch (syntaxError) {
-      logger.error('Syntax error in query parser', syntaxError)
-      response.statusCode = 400;
-      return { errors: [syntaxError] };
-    }
-  })
-  next();
-};
-
 app.use(
   "/graphql",
   bodyParser.json(),
-  queryDirectiveMiddleware,
+  queryDirectiveMiddleware(schema),
   async (request, response, next) => {
     let page;
     if (response.locals.queryConfig && response.locals.queryConfig.testPageUrl) {
