711ce1e4afe69a107d303b655a9000c46def8af8
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2018-03-02 20:02:01 +0100

    Integration testing with Jest and mitmproxy

--- src/browser.js
+++ src/browser.js
@@ -1,20 +1,30 @@
 import puppeteer from 'puppeteer'
 import DataLoader from 'dataloader'
+import { URL } from "url"
+import fse from "fs-extra"
+import path from "path"
 import logger from './logger'
 
-export default puppeteer.launch({
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox'
-  ]
-}).then(browser => new DataLoader(async urls => urls.map(async url => {
-    const browserProfiler = logger.startTimer();
-    const page = await browser.newPage()
-    browserProfiler.done('Initialized new browser page');
+export default puppeteer
+  .launch({
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--proxy-server=127.0.0.1:8000"
+    ]
+  })
+  .then(
+    browser =>
+      new DataLoader(async urls =>
+        urls.map(async url => {
+          const browserProfiler = logger.startTimer();
+          const page = await browser.newPage();
+          browserProfiler.done("Initialized new browser page");
 
-    const pageProfiler = logger.startTimer();
-    await page.goto(url, { waitUntil: 'networkidle2' })
-    pageProfiler.done('Loaded url: ' + url)
-    return page
-  }))
-)
+          const pageProfiler = logger.startTimer();
+          await page.goto(url, { waitUntil: "networkidle2" });
+          pageProfiler.done("Loaded url: " + url);
+          return page;
+        })
+      )
+  );
